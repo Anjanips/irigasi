008208083a5dda62acc0c5844d50d438087a0569
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-02-12 09:42:58 +0700

    Add files via upload

--- pages/iksi.vue
+++ pages/iksi.vue
@@ -1,55 +1,55 @@
 <template>
-    <div class="judul m-5 text-center">
-        <h1>REKAP IKSI TAHUN 2024 UPTD PSDA WILAYAH SUNGAI CITANDUY </h1>
-        <h2>Periode  Tanggal 1 Februari s/d 15 Februari 2025</h2>
-    </div>
-    <table class="table table-bordered">
-  <thead>
-    <tr class="table-success">
-      <th scope="col">NO</th>
-      <th scope="col">NAMA DAERAH IRIGASI</th>
-      <th scope="col">KABUPATEN / KOTA</th>
-      <th scope="col">LUAS AREAL (ha)</th>
-      <th scope="col">INDEKS KINERJA SISTEM IRIGASI(%)</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">VI</th>
-      <td>UPTD PSDA WS. CITANDUY</td>
-      <td></td>
-      <td>5.574,95</td>
-    </tr>
-    <tr class="table-success">
-      <th scope="row">I</th>
-      <td>SUP Citanduy Hulu</td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <th scope="row">1</th>
-      <td>DI. BARANANGSIANG</td>
-      <td>Kabupaten/Kota Tasikmalaya</td>
-      <td>182.77</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>DI. MUHARA</td>
-      <td>Kabupaten Tasikmalaya</td>
-      <td>265,07</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>DI. CIMULU</td>
-      <td>Kabupaten/Kota Tasikmalaya</td>
-      <td>1.546,00</td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>DI. CIGEDE</td>
-      <td>Kabupaten/Kota Tasikmalaya</td>
-      <td>542,00</td>
-    </tr>
-  </tbody>
-  </table>
+  <div class="judul m-5 text-center">
+      <h1>REKAP IKSI TAHUN 2024 UPTD PSDA WILAYAH SUNGAI CITANDUY </h1>
+      <h2>Periode  Tanggal 1 Februari s/d 15 Februari 2025</h2>
+  </div>
+  <table class="table table-bordered">
+<thead>
+  <tr class="table-success">
+    <th scope="col">NO</th>
+    <th scope="col">NAMA DAERAH IRIGASI</th>
+    <th scope="col">KABUPATEN / KOTA</th>
+    <th scope="col">LUAS AREAL (ha)</th>
+    <th scope="col">INDEKS KINERJA SISTEM IRIGASI(%)</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <th scope="row">VI</th>
+    <td>UPTD PSDA WS. CITANDUY</td>
+    <td></td>
+    <td>5.574,95</td>
+  </tr>
+  <tr class="table-success">
+    <th scope="row">I</th>
+    <td>SUP Citanduy Hulu</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <th scope="row">1</th>
+    <td>DI. BARANANGSIANG</td>
+    <td>Kabupaten/Kota Tasikmalaya</td>
+    <td>182.77</td>
+  </tr>
+  <tr>
+    <th scope="row">2</th>
+    <td>DI. MUHARA</td>
+    <td>Kabupaten Tasikmalaya</td>
+    <td>265,07</td>
+  </tr>
+  <tr>
+    <th scope="row">3</th>
+    <td>DI. CIMULU</td>
+    <td>Kabupaten/Kota Tasikmalaya</td>
+    <td>1.546,00</td>
+  </tr>
+  <tr>
+    <th scope="row">4</th>
+    <td>DI. CIGEDE</td>
+    <td>Kabupaten/Kota Tasikmalaya</td>
+    <td>542,00</td>
+  </tr>
+</tbody>
+</table>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="sambutan-container d-flex align-items-center mt-4">
+  <section class="sambutan-container d-flex align-items-center mt-4 ">
       <div class="sambutan-wrapper p-4 rounded-4 shadow-sm "> 
-        <!-- Foto kepala sekolah -->
+        <!-- Logo -->
         <div class="photo-container me-4"> 
           <img src="assets/img/logopemda.png" alt="logo" class="photo" />
         </div>
@@ -16,8 +16,8 @@
           </p>
         </div>
       </div>
-    </section>
-    <div class="container pt-5"> <!-- Mengurangi padding-top di container -->
+  </section>
+    <div class="container pt-4"> <!-- Mengurangi padding-top di container -->
       <div class="row justify-content-center">
         <div class="col">
           <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
@@ -47,38 +47,18 @@
             </div>
   
             <!-- Tombol navigasi -->
-            <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
               <span class="carousel-control-prev-icon rounded-5 bg-secondary" aria-hidden="true"></span>
               <span class="visually-hidden">Previous</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
               <span class="carousel-control-next-icon rounded-5 bg-secondary" aria-hidden="true"></span>
               <span class="visually-hidden">Next</span>
-            </button> -->
+            </button>
           </div>
         </div>
       </div>
     </div>
-  
-    <!-- Seksi sambutan -->
-    <!-- <section class="sambutan-container d-flex align-items-center mt-4">
-      <div class="sambutan-wrapper p-4 rounded-4 shadow-sm ">  -->
-        <!-- Foto kepala sekolah -->
-        <!-- <div class="photo-container me-4"> 
-          <img src="assets/img/logopemda.png" alt="Foto Kepala Sekolah" class="photo" />
-        </div> -->
-        <!-- Teks sambutan -->
-        <!-- <div class="text-container">
-          <header class="sambutan-header">
-            <h1>CAI TEPI SAWAH</h1>
-            <h3>UPTD PSDA WS CITANDUY DINAS SUMBER DAYA AIR PROVINSI JAWA BARAT</h3>
-          </header>
-          <p>
-            Capaian Informasi terkait Pengolahan Irigasi pada Wilayah Sungai Citanduy
-          </p>
-        </div>
-      </div>
-    </section> -->
     <div id="profil" class="container">
           <div class="section">
               <h2>Profile</h2>
