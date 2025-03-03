7cb2bf7f20ece1de5d1c7eeee210ed4f1befccb4
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-03-03 11:59:20 +0700

    Add files via upload

--- pages/pep.vue
+++ pages/pep.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="card text-bg-dark">
-  <img src="assets/img/01.PNG" class="card-img" alt="...">
+  <img src="..." class="card-img" alt="...">
   <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small>Last updated 3 mins ago</small></p>
   </div>
 </div>
 </template>
