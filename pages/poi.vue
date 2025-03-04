7a724dc0f84b12b1ab54feeecad53cf1656fc8d3
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-03-04 10:10:00 +0700

    Add files via upload

--- pages/poi.vue
+++ pages/poi.vue
@@ -1,3 +1,79 @@
 <template>
-    
+    <div class="judul m-5 text-center">
+        <h2>PETUNJUK OPERASI</h2>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss1.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss2.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss3.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss4.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss5.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss6.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss7.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss8.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss9.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss10.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss11.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/ss12.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="caRd text-bg-dark">
+        <img src="assets/img/ss13.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
 </template>
+
+<style scoped>
+
+.img{
+    width: 100px;
+    height: 50px;
+}
+
+</style>
