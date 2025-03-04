06edb4d065ce169d64a83ff31b01c99710928efb
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-03-04 09:11:42 +0700

    Add files via upload

--- pages/pep.vue
+++ pages/pep.vue
@@ -3,117 +3,117 @@
         <h2>PETUNJUK OPERASI</h2>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc1.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc1.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc2.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc2.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc3.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc3.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc4.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc4.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc5.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc5.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc6.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc6.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc7.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc7.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc8.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc8.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc9.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc9.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc10.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc10.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc11.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc11.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc12.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc12.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="caRd text-bg-dark">
+        <img src="assets/img/sc13.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc13.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc14.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc14.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc15.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc15.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc16.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc16.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc17.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc17.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc18.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc18.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc19.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc19.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc20.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc20.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc21.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc21.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc22.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
     <div class="card text-bg-dark">
-        <img src="assets/img/sc22.PNG" class="card-img" alt="...">
-        <div class="card-img-overlay">
-        </div>
-    </div>
-    <div class="card text-bg-dark">
-        <img src="assets/img/sc23.PNG" class="card-img" alt="...">
+        <img src="assets/img/sc23.jpg" class="card-img" alt="...">
         <div class="card-img-overlay">
         </div>
     </div>
