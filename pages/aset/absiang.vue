691a2f8d438f1179c8ccddaaa941054f573843d3
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-03-05 09:56:38 +0700

    Add files via upload

--- pages/aset/absiang.vue
+++ pages/aset/absiang.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card mb-3">
+    <div class="card">
             <img src="assets/img/abaranangsiang.jpg" class="card-img-top " alt="..." style="height: 800px; width: 800px;">
         </div>
 </template>
