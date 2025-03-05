7bb20eb867e1a6aefb212ca7648a4dd24ae3fdb4
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-03-05 10:20:32 +0700

    Add files via upload

--- pages/aset/alaksana.vue
+++ pages/aset/alaksana.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="card mb-3">
-            <img src="assets/img/alaksanaharja.jpg" class="card-img-top" alt="...">
+            <img src="assets/img/alaksanaharja.jpg" class="card-img-top" alt="..." style="height: 800px; width: 800px;">
         </div>
 </template>
--- pages/aset/awd.vue
+++ pages/aset/awd.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="card mb-3">
-            <img src="assets/img/awd.jpg" class="card-img-top" alt="...">
+            <img src="assets/img/awd.jpg" class="card-img-top" alt="..." style="height: 800px; width: 800px;">
         </div>
 </template>
