24c84a8676045331376c0f4982d82dc771dcb610
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-03-06 08:21:34 +0700

    Add files via upload

---
+++ pages/gambarp.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="judul m-5 text-center">
+        <h2>GAMBAR PEMELIHARAAN</h2>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp1.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp2.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp3.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp4.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp5.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp6.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp7.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp8.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp9.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp10.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+    <div class="card text-bg-dark">
+        <img src="assets/img/gp11.jpg" class="card-img" alt="...">
+        <div class="card-img-overlay">
+        </div>
+    </div>
+</template>
