e536c9854ade4a2c8390e9ac23b5a5b3001e8e88
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-03-06 09:19:06 +0700

    Add files via upload

--- pages/aset/absiang.vue
+++ pages/aset/absiang.vue
@@ -1,10 +1,8 @@
 <template>
     <div class="container">
-        <div class="position-relative">
-            <div class="position-absolute top-50 start-50 translate-middle">
-                    <div class="card mb-4">
-                        <img src="assets/img/abaranangsiang.jpg" class="card-img" alt="..." style="height: 800px; width: 800px;">
-                    </div>
+        <div class="justify-content-center">
+            <div class="card mb-4">
+                <img src="assets/img/abaranangsiang.jpg" class="card-img" alt="..." style="height: 800px; width: 800px;">
             </div>
         </div>
     </div>
