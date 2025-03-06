0545bd3a8592a78145540724678a27e156642098
Anjanips <143574069+Anjanips@users.noreply.github.com>
2025-03-06 08:58:44 +0700

    Add files via upload

--- pages/aset/absiang.vue
+++ pages/aset/absiang.vue
@@ -2,8 +2,8 @@
     <!-- <div class="card mb-4">
         <img src="assets/img/abaranangsiang.jpg" class="card-img" alt="..." style="height: 800px; width: 800px;">
     </div> -->
-    <div class="card text-bg-dark">
-        <img src="assets/img/abaranangsiang.jpg" class="card-img" alt="..." style="height: 800px; width: 800px;">
+    <div class="card align-center">
+        <img src="assets/img/abaranangsiang.jpg" class="card-img align-center" alt="..." style="height: 800px; width: 800px;">
         <div class="card-img-overlay">
         </div>
     </div>
